21d35059687fd11baeac5ba299f77968d2f5e57c
Vova Melnyk <hagmen4339@gmail.com>
2021-08-04 21:53:49 +0300

    add module 11 lesson 2

--- Module_11_lesson_2/index.js
+++ Module_11_lesson_2/index.js
@@ -1,5 +1,112 @@
 // Template
 
+// const time1 = new Date("Aug 9, 1995");
+// const time2 = Date.parse("Aug 9, 1995");
+
+// console.log(`time1`, time1);
+// console.log(`time2`, time2);
+
+// const getUserData = new Promise((resolve) => {
+//   resolve({ name: "Bob", age: 12 });
+// });
+
+// const test = Promise.resolve("Great");
+
+// getUserData.then((data) => console.log(data)).catch(() => console.log("error"));
+
+// test.then((data) => console.log(data));
+
+// const createArray = new Promise((resolve, reject) => {
+//   const arr = [];
+
+//   for (let i = 0; i < 5; i++) {
+//     arr.push(i);
+//   }
+
+//   if (arr.length > 10) {
+//     resolve(arr);
+//   } else {
+//     reject(2);
+//   }
+// });
+
+// createArray
+//   .then((result) => console.log(result))
+//   .catch((number) => console.log(number));
+
+// let loaderStatus = false;
+
+function createArrayWithPromise(arraySize) {
+  loaderStatus = true;
+  return new Promise((resolve, reject) => {
+    const arr = [];
+
+    for (let i = 0; i < arraySize; i++) {
+      arr.push(i);
+    }
+
+    if (arr.length > 10) {
+      resolve(arr);
+    } else {
+      reject("error");
+    }
+  });
+}
+
+// let finalResult = null;
+
+// function countTotalArray(array) {
+//   return array.reduce((acc, number) => acc + number);
+// }
+
+// const mapArray = (arr) => arr.map((number) => number * 2);
+
+// createArrayWithPromise(10000000)
+//   .then((result) => result)
+//   .then(mapArray)
+//   //   .then((doubleArr) => doubleArr.reduce((acc, number) => acc + number))
+//   .then(countTotalArray)
+//   .then((reduceResult) => (finalResult = reduceResult))
+//   .catch((err) => console.log(err));
+// //   .finally(() => console.log("hello"));
+
+// console.log(finalResult);
+
+// createArrayWithPromise(5)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+// //   .finally(() => console.log("hello"));
+
+// Promise.all([
+//   createArrayWithPromise(5),
+//   createArrayWithPromise(20),
+//   createArrayWithPromise(35),
+// ])
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+// [promise1Result, promise2Result, promise3Result]
+// Promise.race([
+//   createArrayWithPromise(20),
+//   createArrayWithPromise(35),
+//   createArrayWithPromise(15),
+// ])
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+// Promise.allSettled([
+//   createArrayWithPromise(5),
+//   createArrayWithPromise(20),
+//   createArrayWithPromise(35),
+// ])
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// promise
+//   .then()
+
+//   [(1, 2, 3)].map((number, ind, arr) => number * 2);
+// [1, 2, 3].map((number) => number - 2);
+// [1, 2, 3].map((number) => number + 2);
+
 // const firstPromise = new Promise((ok, err) =>{
 // })
 
